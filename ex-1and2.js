3ce5240cf78a60211fe3954b50a659fb1df53255
April Twenty <atcahriya.nna@gmail.com>
2024-08-08 21:55:53 +0700

    Answer exam question

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,14 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter";
+let LastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Krual Bang Kruai";
 
+const restaurantName = "Chef's Table by Chef James";
+const netPrice = 15000;
+const totalRestaurant = 1500;
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+netPrice = 5000;
+totalRestaurant = 2500;
